b632575e025cc5986def169a6ba89311743d03fd
urantiacos <luis.ibarra@urantiacos.cl>
2015-11-16 01:28:37 -0300

    upload
    se carga la versión del sitio al 16-11-2015

---
+++ contact-form.php
@@ -0,0 +1,46 @@
+﻿<?php
+
+$to="demo@email.com";/*Your Email*/
+
+$subject="Inscripción Primer Congreso Iberoamericano de Éticas Aplicadas";
+
+$date=date("l, F jS, Y");
+$time=date("h:i A");
+
+$type=$_REQUEST['type'];
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$message=$_REQUEST['message'];
+
+$msg  = "";
+$msg .= "Message sent from website on date:  $date, hour: $time.\n";
+$msg .= "Email: $email\n";
+
+
+if($name == "") {
+	echo "<div class='alert alert-danger'>
+		<a class='close' data-dismiss='alert'>×</a>
+		<strong>Warning!</strong> Please fill the Name field.
+	</div>";
+
+} else if($email=="") {
+	echo "<div class='alert alert-danger'>
+		<a class='close' data-dismiss='alert'>×</a>
+		<strong>Warning!</strong> Please fill the Email field.
+	</div>";
+
+} else if($message == "" && $type == 'contact') {
+	echo "<div class='alert alert-danger'>
+		<a class='close' data-dismiss='alert'>×</a>
+		<strong>Warning!</strong> Please fill the Message field.
+	</div>";
+
+} else {
+	mail($to,$subject,$msg,"From:".$email);
+	echo "<div class='alert alert-success'>
+		<a class='close' data-dismiss='alert'>×</a>
+		<strong>Thank you!</strong>
+	</div>";
+}
+
+?>
